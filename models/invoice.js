f364917eee7cf2b5a2a8ea41fea07867f6cb6416
Ansar Qazi <ansarqazi10@gmail.com>
2023-01-08 23:43:25 +0500

    added amount for tasks in invoice

--- models/invoice.js
+++ models/invoice.js
@@ -14,6 +14,7 @@ const schema = new mongoose.Schema({
       color: { type: String, required: true },
       date: { type: String, required: true },
       time: { type: String, required: true },
+      amount: { type: String, required: true },
     },
   ],
   project: {
